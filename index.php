e43a554bf71e794f4eda33a14a76f4c1fd569b57
Christophe <cavo789@yahoo.com>
2018-12-31 16:09:18 +0100

    Abandonment of jQuery and migration to vue.js

--- index.php
+++ index.php
@@ -12,15 +12,12 @@
  * This version will make easier to read and understand the formula
  *
  * @see excelFormulaUtilitiesJS on https://github.com/joshbtn/excelFormulaUtilitiesJS
+ *
+ * Last mod:
+ * 2018-12-31 - Abandonment of jQuery and migration to vue.js
  */
 
 define('REPO', 'https://github.com/cavo789/excel_formatter');
-
-// Sample formula
-$formula = '=IF(ISNA(VLOOKUP("Value";G1:K11;1;FALSE));"Not found";"Found")';
-
-// Delimiter
-$delim = ';';
 
 // Get the GitHub corner
 $github = '';
@@ -31,85 +28,102 @@ if (is_file($cat = __DIR__ . DIRECTORY_SEPARATOR . 'octocat.tmpl')) {
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8"/>
-        <meta name="author" content="Christophe Avonture" />
-        <meta name="robots" content="noindex, nofollow" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8;" />
-        <title>Excel formula beautifier</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/css/screen.css">
-    </head>
-    <body>
-        <?php echo $github; ?>
-        <div class="container">
-            <div class="page-header"><h1>Excel formula beautifier</h1></div>
-            <div class="container">
-                <div class="form-group">
-                    <label for="formula">Copy/Paste your Excel's formula in the 
-                        textbox below then click on the Beautify button:</label>
-                    <details>
-                    <summary>How to use?</summary>
-                    <div class="row">
-                            <div class="col-sm">
-                                <ul>
-                                    <li>Type (or paste) the Excel formula to explain</li>
-                                    <li>And click on the Beautify button.</li>
-                                </ul>
-                            </div>
-                            <div class="col-sm">
-                                <img src="https://raw.githubusercontent.com/cavo789/excel_formatter/master/images/demo.gif" alt="Demo">
-                            </div>
-                        </div>
-                    </div>
-                </details>
-                    <textarea class="form-control" rows="3" id="formula" name="formula"><?php echo $formula; ?></textarea>
-                </div>
-                <div class="form-group row">
-                    <label for="delim">Excel formula delimiter:</label>&nbsp;
-                    <input type="text" style="width:30px;" size="3" value="<?php echo $delim; ?>" class="form-control" id="delim">
-                </div>
-                <button type="button" id="btnBeautify" class="btn btn-primary">Beautify</button>
-                <hr/>
-                <pre id="Result"></pre>
-                <i style="display:block;font-size:0.6em;">
-                    <a href="https://github.com/joshbtn/excelFormulaUtilitiesJS">
-                        Excel beautifier written by Josh Bennett
-                    </a>
-                </i>
-            </div>
-        </div>
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="assets/js/excel-formula.min.js"></script>
-        <script type="text/javascript">
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="author" content="Christophe Avonture" />
+		<meta name="robots" content="noindex, nofollow" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=9; IE=8;" />
+		<title>Excel formula beautifier</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/css/screen.css">
+	</head>
+	<body>
+		<?php echo $github; ?>
+		<div class="container">
+			<div class="page-header"><h1>Excel formula beautifier</h1></div>
+			<div class="container" id="app">
+				<div class="form-group">
+					<label for="formula">Copy/Paste your Excel's formula in the 
+						textbox below then click on the Beautify button:</label>
+					<how-to-use demo="https://raw.githubusercontent.com/cavo789/excel_formatter/master/images/demo.gif"></how-to-use>
+					<textarea class="form-control" rows="3" name="formula" v-model="formula"></textarea>
+				</div>
+				<div class="form-group row">
+					<label for="delim">Excel formula delimiter:</label>&nbsp;
+					<input type="text" style="width:30px;" size="3" v-model="delim" class="form-control">
+				</div>
+				<button type="button" class="btn btn-primary" @click="processBeautify">Beautify</button>
+				<hr/>
+				<pre v-html="HTML"></span>
+				<i style="display:block;font-size:0.6em;">
+					<a href="https://github.com/joshbtn/excelFormulaUtilitiesJS">
+						Excel beautifier written by Josh Bennett
+					</a>
+				</i>
+			</div>
+		</div>
+		<script src="https://unpkg.com/vue"></script>
+		<script type="text/javascript" src="assets/js/excel-formula.min.js"></script>
+		<script type="text/javascript">
 
-            String.prototype.replaceAll = function(search, replacement) {
-                var target = this;
-                return target.replace(new RegExp(search, 'g'), replacement);
-            };
+			String.prototype.replaceAll = function(search, replacement) {
+				var target = this;
+				return target.replace(new RegExp(search, 'g'), replacement);
+			};
 
-            $('#btnBeautify').click(function(e)  {
+			Vue.component('how-to-use', {
+				props: {
+					demo: {
+						type: String,
+						required: true
+					}
+				},
+				template:
+					`<details>
+						<summary>How to use?</summary>
+						<div class="row">
+								<div class="col-sm">
+									<ul>
+										<li>Type (or paste) the Excel formula to explain</li>
+										<li>And click on the Beautify button.</li>
+									</ul>
+								</div>
+								<div class="col-sm"><img v-bind:src="demo" alt="Demo"></div>
+							</div>
+						</div>
+					</details>`
+			});
 
-                e.stopImmediatePropagation();
+			var app = new Vue({
+				el: '#app',
+				data: {
+					formula: '=IF(ISNA(VLOOKUP("Value";G1:K11;1;FALSE));"Not found";"Found")',
+					delim: ';',
+					HTML: ''
+				},
+				methods: {
+					processBeautify() {
 
-                var formula = $('#formula').val();
-                if ($('#delim').val()==";") {
-                    formula = formula.replaceAll(";", ",");
-                }
+						this.formula = this.formula.trim();
 
-                // Call the beautifier
-                var formattedFormula = excelFormulaUtilities.formatFormulaHTML(formula);
+						if (this.delim == ";") {
+							this.formula = this.formula.replaceAll(";", ",");
+						}
 
-                if ($('#delim').val()==";") {
-                    formattedFormula = formattedFormula.replaceAll(",", ";");
-                }
-                
-                // And output the result in the DOM element
-                $('#Result').html(formattedFormula);
-            });
-        </script>
-    </body>
+						// Call the beautifier
+						var formattedFormula = excelFormulaUtilities.formatFormulaHTML(this.formula);
+
+						if (this.delim == ";") {
+							formattedFormula = formattedFormula.replaceAll(",", ";");
+						}
+						
+						// And output the result in the DOM element
+						this.HTML = formattedFormula;
+					}
+				}
+			});
+		</script>
+	</body>
 </html>
